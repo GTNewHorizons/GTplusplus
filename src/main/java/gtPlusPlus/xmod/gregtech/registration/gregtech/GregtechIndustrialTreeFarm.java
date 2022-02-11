fe4aa9fe57ef8366db6533db35720b1d39978065
Alkalus <3060479+draknyte1@users.noreply.github.com>
2022-02-11 21:54:12 +0000

    Added NEI handler for TGS. Added BoP support to TGS. Added base work for LFH.

--- src/main/java/gtPlusPlus/xmod/gregtech/registration/gregtech/GregtechIndustrialTreeFarm.java
+++ src/main/java/gtPlusPlus/xmod/gregtech/registration/gregtech/GregtechIndustrialTreeFarm.java
@@ -1,8 +1,10 @@
 
 package gtPlusPlus.xmod.gregtech.registration.gregtech;
 
+import gregtech.api.util.GT_Recipe;
 import gtPlusPlus.api.objects.Logger;
 import gtPlusPlus.core.lib.CORE;
+import gtPlusPlus.core.util.minecraft.RecipeUtils;
 import gtPlusPlus.xmod.gregtech.api.enums.GregtechItemList;
 import gtPlusPlus.xmod.gregtech.common.tileentities.machines.multi.production.GregtechMetaTileEntityTreeFarm;
 
@@ -10,8 +12,8 @@ public class GregtechIndustrialTreeFarm {
 
 	public static void run() {
 		if (gtPlusPlus.core.lib.LoadedMods.Gregtech) {
-			Logger.INFO("Gregtech5u Content | Registering Tree Farm Multiblock.");
 			if (CORE.ConfigSwitches.enableMultiblock_TreeFarmer) {
+				Logger.INFO("Gregtech5u Content | Registering Tree Farm Multiblock.");
 				run1();
 			}
 		}
@@ -19,10 +21,16 @@ public class GregtechIndustrialTreeFarm {
 	}
 
 	private static void run1() {
-		// Industrial Maceration Stack Multiblock
 		GregtechItemList.Industrial_TreeFarm
 				.set(new GregtechMetaTileEntityTreeFarm(836, "treefarm.controller.tier.single", "Tree Growth Simulator")
 						.getStackForm(1L));
-
+		GregtechMetaTileEntityTreeFarm.loadMapWoodFromSapling();
+		Logger.INFO("Dumping Tree Growth Simulations.");
+		for (GT_Recipe aRecipe : gregtech.api.util.GTPP_Recipe.GTPP_Recipe_Map.sTreeSimFakeRecipes.mRecipeList) {
+			String[] aInfo = RecipeUtils.getRecipeInfo(aRecipe);
+			for (String s : aInfo) {
+				Logger.INFO(s);
+			}
+		}
 	}
 }
