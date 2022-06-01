e4245b0ad3327e18dd2368f12208d205cad49ae5
miozune <miozune@gmail.com>
2022-06-01 14:25:44 +0900

    Fix height in tooltip

--- src/main/java/gtPlusPlus/xmod/gregtech/common/tileentities/machines/multi/production/GregtechMTE_FrothFlotationCell.java
+++ src/main/java/gtPlusPlus/xmod/gregtech/common/tileentities/machines/multi/production/GregtechMTE_FrothFlotationCell.java
@@ -69,7 +69,7 @@ public class GregtechMTE_FrothFlotationCell extends GregtechMeta_MultiBlockBase<
 				.addInfo("Process that milled ore!")
 				.addPollutionAmount(getPollutionPerSecond(null))
 				.addSeparator()
-				.beginStructureBlock(3, 3, 3, true)
+				.beginStructureBlock(3, 9, 3, true)
 				.addController("Front Center")
 				.addCasingInfo("Inconel Reinforced Casing", 68)
 				.addCasingInfo("Flotation Casing", 52)
