35f64fac8586841374b305b2dc861505be700a45
prts44 <ryanckit420@gmail.com>
2022-06-15 23:05:12 -0400

    Change how much is gained per fertilizer

--- src/main/java/gtPlusPlus/xmod/gregtech/common/tileentities/machines/basic/GT_MetaTileEntity_CropHarvestor.java
+++ src/main/java/gtPlusPlus/xmod/gregtech/common/tileentities/machines/basic/GT_MetaTileEntity_CropHarvestor.java
@@ -421,7 +421,7 @@ public class GT_MetaTileEntity_CropHarvestor extends GT_MetaTileEntity_BasicTank
 			return false;
 		} else {
 			//Logger.INFO("Current Nutrient: "+aCrop.getNutrientStorage()+" for "+aCrop.getCrop().displayName());
-			aCrop.setNutrientStorage(200);
+			aCrop.setNutrientStorage(getNutrientStorage() + 100);
 			return true;
 		}
 	}
