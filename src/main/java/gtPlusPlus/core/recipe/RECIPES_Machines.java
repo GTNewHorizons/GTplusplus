c80aa94fd8db9f24044af50077818d317c4af71f
Alexandre Tardif <alexandre.tardif@nexusinno.com>
2022-08-09 23:21:26 -0400

    Disable the large generator array

--- src/main/java/gtPlusPlus/core/recipe/RECIPES_Machines.java
+++ src/main/java/gtPlusPlus/core/recipe/RECIPES_Machines.java
@@ -263,7 +263,6 @@ public class RECIPES_Machines {
 		gt4Redstone();
 		gt4Inventory();
 		
-		multiGeneratorArray();
 		multiForgeHammer();
 		multiMolecularTransformer();
 		multiXlTurbines();
@@ -465,19 +464,6 @@ public class RECIPES_Machines {
 			aTier++;
 		}
 		
-	}
-
-	private static void multiGeneratorArray() {
-
-        GT_ModHandler.addCraftingRecipe(
-        		GregtechItemList.Generator_Array_Controller.get(1L),
-        		CI.bitsd, 
-        		new Object[]{"CTC", "FMF", "CBC",
-        				'M', CI.getTieredGTPPMachineCasing(4, 1),
-        				'B', OrePrefixes.pipeHuge.get(Materials.StainlessSteel),
-        				'C', OrePrefixes.circuit.get(Materials.Data),
-        				'F', ItemList.Electric_Pump_EV,
-        				'T', CI.getSensor(4, 1)});
 	}
 
 	private static void multiForgeHammer() {
