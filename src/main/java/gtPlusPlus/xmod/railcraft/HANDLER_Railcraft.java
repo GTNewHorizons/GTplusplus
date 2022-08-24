a79833d1980c24eafab47f6dea4dcc132f60f689
くぁーりぃ <kaito6343@gmail.com>
2022-08-25 04:24:17 +0900

    add GTNHCore check

--- src/main/java/gtPlusPlus/xmod/railcraft/HANDLER_Railcraft.java
+++ src/main/java/gtPlusPlus/xmod/railcraft/HANDLER_Railcraft.java
@@ -100,11 +100,13 @@ public class HANDLER_Railcraft {
 				RailcraftUtils.addCokeOvenRecipe(aInputs2[i], true, true, aOutputs[i], FluidUtils.getFluidStack("creosote", 30), 500);			
 			}
 			
-			for (int i=0;i<aOutputs.length;i++) {
-				RailcraftUtils.addAdvancedCokeOvenRecipe(aInputs1[i], true, true, aInputs2[i], 20);
-			}
-			for (int i=0;i<aOutputs.length;i++) {
-				RailcraftUtils.addAdvancedCokeOvenRecipe(aInputs2[i], true, true, aInputs2[i], 20);
+			if(LoadedMods.DreamCraft){
+				for (int i=0;i<aOutputs.length;i++) {
+					RailcraftUtils.addAdvancedCokeOvenRecipe(aInputs1[i], true, true, aInputs2[i], 20);
+				}
+				for (int i=0;i<aOutputs.length;i++) {
+					RailcraftUtils.addAdvancedCokeOvenRecipe(aInputs2[i], true, true, aInputs2[i], 20);
+				}
 			}
 		}
 	}
