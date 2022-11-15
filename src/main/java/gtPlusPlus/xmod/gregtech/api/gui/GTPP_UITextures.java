894ae7cadfabcb7901911dff9f73e10ea17a0ca7
miozune <miozune@gmail.com>
2022-11-15 19:53:46 +0900

    Boiler

--- src/main/java/gtPlusPlus/xmod/gregtech/api/gui/GTPP_UITextures.java
+++ src/main/java/gtPlusPlus/xmod/gregtech/api/gui/GTPP_UITextures.java
@@ -6,6 +6,12 @@ import com.gtnewhorizons.modularui.api.drawable.UITexture;
 
 public class GTPP_UITextures {
 
+    public static final UITexture OVERLAY_SLOT_COAL = UITexture.fullImage(MODID, "gui/overlay_slot/coal");
+    public static final UITexture OVERLAY_SLOT_CANISTER_DARK =
+            UITexture.fullImage(MODID, "gui/overlay_slot/canister_dark");
+
     public static final UITexture PROGRESSBAR_FLUID_REACTOR =
             UITexture.fullImage(MODID, "gui/progressbar/fluid_reactor");
+    public static final UITexture PROGRESSBAR_BOILER_EMPTY = UITexture.fullImage(MODID, "gui/progressbar/boiler_empty");
+    public static final UITexture PROGRESSBAR_FUEL = UITexture.fullImage(MODID, "gui/progressbar/fuel");
 }
