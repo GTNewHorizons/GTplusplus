af211217f8e0e064507a1c597f6a2b07d5744b8a
miozune <miozune@gmail.com>
2022-11-21 12:39:25 +0900

    Crop Manager

--- src/main/java/gtPlusPlus/xmod/gregtech/api/gui/GTPP_UITextures.java
+++ src/main/java/gtPlusPlus/xmod/gregtech/api/gui/GTPP_UITextures.java
@@ -9,9 +9,14 @@ public class GTPP_UITextures {
     public static final UITexture OVERLAY_SLOT_COAL = UITexture.fullImage(MODID, "gui/overlay_slot/coal");
     public static final UITexture OVERLAY_SLOT_CANISTER_DARK =
             UITexture.fullImage(MODID, "gui/overlay_slot/canister_dark");
+    public static final UITexture OVERLAY_SLOT_WEED_EX = UITexture.fullImage(MODID, "gui/overlay_slot/weed_ex");
+    public static final UITexture OVERLAY_SLOT_FERTILIZER = UITexture.fullImage(MODID, "gui/overlay_slot/fertilizer");
 
     public static final UITexture PROGRESSBAR_FLUID_REACTOR =
             UITexture.fullImage(MODID, "gui/progressbar/fluid_reactor");
     public static final UITexture PROGRESSBAR_BOILER_EMPTY = UITexture.fullImage(MODID, "gui/progressbar/boiler_empty");
     public static final UITexture PROGRESSBAR_FUEL = UITexture.fullImage(MODID, "gui/progressbar/fuel");
+
+    public static final UITexture OVERLAY_BUTTON_HARVESTER_MODE =
+            UITexture.fullImage(MODID, "gui/overlay_button/harvester_mode");
 }
