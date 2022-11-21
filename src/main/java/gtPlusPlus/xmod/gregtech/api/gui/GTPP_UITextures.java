c94e1f431d4b4c91645b426c70210ee44c1280a5
miozune <miozune@gmail.com>
2022-11-21 17:56:51 +0900

    Bronze Workbench & Advanced Workbench

--- src/main/java/gtPlusPlus/xmod/gregtech/api/gui/GTPP_UITextures.java
+++ src/main/java/gtPlusPlus/xmod/gregtech/api/gui/GTPP_UITextures.java
@@ -6,11 +6,26 @@ import com.gtnewhorizons.modularui.api.drawable.UITexture;
 
 public class GTPP_UITextures {
 
+    public static final UITexture BUTTON_STANDARD_BRONZE = UITexture.fullImage(MODID, "gui/button/standard_bronze");
+
     public static final UITexture OVERLAY_SLOT_COAL = UITexture.fullImage(MODID, "gui/overlay_slot/coal");
     public static final UITexture OVERLAY_SLOT_CANISTER_DARK =
             UITexture.fullImage(MODID, "gui/overlay_slot/canister_dark");
     public static final UITexture OVERLAY_SLOT_WEED_EX = UITexture.fullImage(MODID, "gui/overlay_slot/weed_ex");
     public static final UITexture OVERLAY_SLOT_FERTILIZER = UITexture.fullImage(MODID, "gui/overlay_slot/fertilizer");
+    public static final UITexture OVERLAY_SLOT_ELECTRIC_TOOL =
+            UITexture.fullImage(MODID, "gui/overlay_slot/electric_tool");
+    public static final UITexture OVERLAY_SLOT_PAGE_PRINTED_BRONZE =
+            UITexture.fullImage(MODID, "gui/overlay_slot/page_printed_bronze");
+    public static final UITexture OVERLAY_SLOT_ARROW = UITexture.fullImage(MODID, "gui/overlay_slot/arrow");
+    public static final UITexture OVERLAY_SLOT_ARROW_BRONZE =
+            UITexture.fullImage(MODID, "gui/overlay_slot/arrow_bronze");
+    public static final UITexture OVERLAY_SLOT_CRAFT_OUTPUT =
+            UITexture.fullImage(MODID, "gui/overlay_slot/craft_output");
+    public static final UITexture OVERLAY_SLOT_CRAFT_OUTPUT_BRONZE =
+            UITexture.fullImage(MODID, "gui/overlay_slot/craft_output_bronze");
+    public static final UITexture OVERLAY_SLOT_PARK = UITexture.fullImage(MODID, "gui/overlay_slot/park");
+    public static final UITexture OVERLAY_SLOT_PARK_BRONZE = UITexture.fullImage(MODID, "gui/overlay_slot/park_bronze");
 
     public static final UITexture PROGRESSBAR_FLUID_REACTOR =
             UITexture.fullImage(MODID, "gui/progressbar/fluid_reactor");
@@ -19,4 +34,9 @@ public class GTPP_UITextures {
 
     public static final UITexture OVERLAY_BUTTON_HARVESTER_MODE =
             UITexture.fullImage(MODID, "gui/overlay_button/harvester_mode");
+    public static final UITexture OVERLAY_BUTTON_FLUSH = UITexture.fullImage(MODID, "gui/overlay_button/flush");
+    public static final UITexture OVERLAY_BUTTON_FLUSH_BRONZE =
+            UITexture.fullImage(MODID, "gui/overlay_button/flush_bronze");
+    public static final UITexture OVERLAY_BUTTON_AUTOMATION =
+            UITexture.fullImage(MODID, "gui/overlay_button/automation");
 }
