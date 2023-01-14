a295bb02977de9a5bfc42928ca3ab6d47f4699f1
minecraft7771 <maxim235@gmx.de>
2023-01-14 22:47:52 +0100

    Fix persistance of structure variables

--- src/main/java/gtPlusPlus/xmod/gregtech/common/tileentities/machines/multi/production/mega/GregTechMetaTileEntity_MegaAlloyBlastSmelter.java
+++ src/main/java/gtPlusPlus/xmod/gregtech/common/tileentities/machines/multi/production/mega/GregTechMetaTileEntity_MegaAlloyBlastSmelter.java
@@ -415,8 +415,8 @@ public class GregTechMetaTileEntity_MegaAlloyBlastSmelter
         if (hasNormalCoils) coilLevel = HeatingCoilLevel.None;
         if (mMaintenanceHatches.size() != 1) return false;
         if (mMufflerHatches.size() != 45) return false;
-        if (this.glassTier < 10 && !this.mEnergyHatches.isEmpty()) {
-            for (GT_MetaTileEntity_Hatch_Energy hatchEnergy : this.mEnergyHatches) {
+        if (this.glassTier < 10 && !getExoticAndNormalEnergyHatchList().isEmpty()) {
+            for (GT_MetaTileEntity_Hatch hatchEnergy : getExoticAndNormalEnergyHatchList()) {
                 if (this.glassTier < hatchEnergy.mTier) {
                     return false;
                 }
