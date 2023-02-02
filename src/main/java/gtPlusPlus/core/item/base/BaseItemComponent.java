15118b271726d4c324d49f3d943832f83062e330
miozune <miozune@gmail.com>
2023-02-02 16:17:05 +0900

    Fix extra spaces appear on item name

--- src/main/java/gtPlusPlus/core/item/base/BaseItemComponent.java
+++ src/main/java/gtPlusPlus/core/item/base/BaseItemComponent.java
@@ -83,17 +83,7 @@ public class BaseItemComponent extends Item {
         }
         registerComponent();
 
-        String aFormattedLangName = componentType.getName();
-        if (!aFormattedLangName.startsWith(" ")) {
-            if (aFormattedLangName.contains("@")) {
-                String[] aSplit = aFormattedLangName.split("@");
-                aFormattedLangName = aSplit[0] + " %material " + aSplit[1];
-            }
-        }
-        if (aFormattedLangName.equals(componentType.getName())) {
-            aFormattedLangName = "%material" + aFormattedLangName;
-        }
-        GT_LanguageManager.addStringLocalization("gtplusplus.item." + unlocalName + ".name", aFormattedLangName);
+        GT_LanguageManager.addStringLocalization("gtplusplus.item." + unlocalName + ".name", getFormattedLangName());
     }
 
     // For Cell Generation
@@ -126,8 +116,16 @@ public class BaseItemComponent extends Item {
                 ItemUtils.getSimpleStack(this));
         registerComponent();
 
-        String aFormattedLangName = componentType.getName().replace("@", "%material");
-        GT_LanguageManager.addStringLocalization("gtplusplus.item." + this.unlocalName + ".name", aFormattedLangName);
+        GT_LanguageManager
+                .addStringLocalization("gtplusplus.item." + this.unlocalName + ".name", getFormattedLangName());
+    }
+
+    private String getFormattedLangName() {
+        String aFormattedLangName = componentType.getName();
+        if (!aFormattedLangName.startsWith(" ")) {
+            return aFormattedLangName.replace("@", "%material");
+        }
+        return "%material" + aFormattedLangName;
     }
 
     public boolean registerComponent() {
