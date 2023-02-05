5bc354838a7fe17f011e85fc1924788aa00ad1f3
miozune <miozune@gmail.com>
2023-02-05 13:50:01 +0900

    Cleanup recipe adder

--- src/main/java/gtPlusPlus/xmod/gregtech/api/interfaces/internal/IGregtech_RecipeAdder.java
+++ src/main/java/gtPlusPlus/xmod/gregtech/api/interfaces/internal/IGregtech_RecipeAdder.java
@@ -6,13 +6,14 @@ import net.minecraftforge.fluids.FluidStack;
 import gregtech.api.enums.Materials;
 import gtPlusPlus.core.material.Material;
 
+@SuppressWarnings("UnusedReturnValue")
 public interface IGregtech_RecipeAdder {
 
     /**
      * Adds a Coke Oven Recipe
      *
      * @param aInput1      = first Input (not null, and respects StackSize)
-     * @param aInputb      = second Input (can be null, and respects StackSize)
+     * @param aInput2      = second Input (can be null, and respects StackSize)
      * @param aFluidOutput = Output of the Creosote (not null, and respects StackSize)
      * @param aFluidInput  = fluid Input (can be null, and respects StackSize)
      * @param aOutput      = Output of the Coal/coke (can be null, and respects StackSize)
@@ -20,8 +21,6 @@ public interface IGregtech_RecipeAdder {
      * @param aEUt         = EU needed for heating up (must be >= 0)
      * @return true if the Recipe got added, otherwise false.
      */
-    // public boolean addCokeOvenRecipe(boolean aOptimize, ItemStack[] aInputs, ItemStack[] aOutputs, Object aSpecial,
-    // FluidStack[] aFluidInputs, FluidStack[] aFluidOutputs, int aDuration, int aEUt, int aSpecialValue);
     public boolean addCokeOvenRecipe(ItemStack aInput1, ItemStack aInput2, FluidStack aFluidInput,
             FluidStack aFluidOutput, ItemStack aOutput, int aDuration, int aEUt);
 
@@ -30,8 +29,6 @@ public interface IGregtech_RecipeAdder {
 
     public boolean addCokeOvenRecipe(ItemStack aInput1, ItemStack aInput2, FluidStack[] aFluidInputs,
             FluidStack[] aFluidOutputs, ItemStack[] aOutputs, int aDuration, int aEUt);
-
-    public boolean addFuel(ItemStack aInput1, ItemStack aOutput1, int aEU, int aType);
 
     /**
      * Adds a Matter Fabricator Recipe
@@ -43,42 +40,6 @@ public interface IGregtech_RecipeAdder {
      * @return true if the Recipe got added, otherwise false.
      */
     public boolean addMatterFabricatorRecipe(FluidStack aFluidInput, FluidStack aFluidOutput, int aDuration, int aEUt);
-
-    /**
-     * Adds a Matter Fabricator Recipe
-     *
-     * @param aItemInput   = ItemStack Input, can be null I assume.
-     * @param aFluidInput  = fluid Input (can be UU_Amp or null, and respects StackSize)
-     * @param aFluidOutput = Output of the UU-Matter (not null, and respects StackSize)
-     * @param aDuration    = Duration (must be >= 0)
-     * @param aEUt         = EU needed for heating up (must be >= 0)
-     * @return true if the Recipe got added, otherwise false.
-     */
-    public boolean addMatterFabricatorRecipe(ItemStack aItemInput, FluidStack aFluidInput, FluidStack aFluidOutput,
-            int aDuration, int aEUt);
-
-    /**
-     * Adds a Recipe for the Dehydrator. (up to 9 Outputs)
-     *
-     * @param aInput       = Input itemstack (not null, and respects StackSize)
-     * @param aFluidInput  = fluid Input (can be UU_Amp or null, and respects StackSize)
-     * @param aOutputItems = Itemstack[] (not null, and respects StackSize)
-     * @param aDuration    = Duration (must be >= 0)
-     * @param aEUt         = EU needed for heating up (must be >= 0)
-     * @return true if the Recipe got added, otherwise false.
-     */
-
-    // public boolean addDehydratorRecipe(ItemStack aInput, FluidStack aFluid, ItemStack[] aOutputItems, int aDuration,
-    // int aEUt);
-    /*
-     * public boolean addDehydratorRecipe(FluidStack aFluid, FluidStack aOutputFluid, ItemStack[] aOutputItems, int
-     * aDuration, int aEUt);
-     */
-    /*
-     * public boolean addDehydratorRecipe(ItemStack aItemA, ItemStack aItemB, ItemStack[] aOutputItems, int aDuration,
-     * int aEUt); public boolean addDehydratorRecipe(ItemStack aItemA, ItemStack aItemB, FluidStack aFluid, ItemStack[]
-     * aOutputItems, FluidStack aOutputFluid, int aDuration, int aEUt);
-     */
 
     /**
      * Adds a Recipe for the Dehydrator. (up to 9 Outputs)
@@ -98,11 +59,11 @@ public interface IGregtech_RecipeAdder {
     /**
      * Adds a Recipe for the Alloy Blast Smelter. (up to 9 Inputs)
      *
-     * @param aInput       = ItemStack[] (not null, and respects StackSize)
-     * @param aFluidOutput = Output of the Molten Metal (not null, and respects StackSize)
-     * @param aChances     = Output Chance (can be == 0)
-     * @param aDuration    = Duration (must be >= 0)
-     * @param aEUt         = EU per tick needed for heating up (must be >= 0)
+     * @param aInput    = ItemStack[] (not null, and respects StackSize)
+     * @param aOutput   = Output of the Molten Metal (not null, and respects StackSize)
+     * @param aChance   = Output Chance (can be == 0)
+     * @param aDuration = Duration (must be >= 0)
+     * @param aEUt      = EU per tick needed for heating up (must be >= 0)
      * @return true if the Recipe got added, otherwise false.
      */
     public boolean addBlastSmelterRecipe(ItemStack[] aInput, FluidStack aOutput, int aChance, int aDuration, int aEUt);
@@ -110,12 +71,12 @@ public interface IGregtech_RecipeAdder {
     /**
      * Adds a Recipe for the Alloy Blast Smelter. (up to 9 Inputs)
      *
-     * @param aInput       = ItemStack[] (not null, and respects StackSize)
-     * @param aFluidInput  = Input of a fluid (can be null, and respects StackSize)
-     * @param aFluidOutput = Output of the Molten Metal (not null, and respects StackSize)
-     * @param aChances     = Output Chance (can be == 0)
-     * @param aDuration    = Duration (must be >= 0)
-     * @param aEUt         = EU per tick needed for heating up (must be >= 0)
+     * @param aInput      = ItemStack[] (not null, and respects StackSize)
+     * @param aInputFluid = Input of a fluid (can be null, and respects StackSize)
+     * @param aOutput     = Output of the Molten Metal (not null, and respects StackSize)
+     * @param aChance     = Output Chance (can be == 0)
+     * @param aDuration   = Duration (must be >= 0)
+     * @param aEUt        = EU per tick needed for heating up (must be >= 0)
      * @return true if the Recipe got added, otherwise false.
      */
     public boolean addBlastSmelterRecipe(ItemStack[] aInput, FluidStack aInputFluid, FluidStack aOutput, int aChance,
@@ -124,12 +85,12 @@ public interface IGregtech_RecipeAdder {
     /**
      * Adds a Recipe for the Alloy Blast Smelter. (up to 9 Inputs, More than 1 Fluids)
      *
-     * @param aInput       = ItemStack[] (not null, and respects StackSize)
-     * @param aFluidInput  = FluidStack[] (can be null, and respects StackSize)
-     * @param aFluidOutput = Output of the Molten Metal (not null, and respects StackSize)
-     * @param aChances     = Output Chance (can be == 0)
-     * @param aDuration    = Duration (must be >= 0)
-     * @param aEUt         = EU per tick needed for heating up (must be >= 0)
+     * @param aInput      = ItemStack[] (not null, and respects StackSize)
+     * @param aInputFluid = FluidStack[] (can be null, and respects StackSize)
+     * @param aOutput     = Output of the Molten Metal (not null, and respects StackSize)
+     * @param aChance     = Output Chance (can be == 0)
+     * @param aDuration   = Duration (must be >= 0)
+     * @param aEUt        = EU per tick needed for heating up (must be >= 0)
      * @return true if the Recipe got added, otherwise false.
      */
     public boolean addBlastSmelterRecipe(ItemStack[] aInput, FluidStack[] aInputFluid, FluidStack aOutput, int aChance,
@@ -139,10 +100,10 @@ public interface IGregtech_RecipeAdder {
      * Adds a Recipe for the Alloy Blast Smelter. (up to 9 Inputs)
      *
      * @param aInput       = ItemStack[] (not null, and respects StackSize)
-     * @param aFluidInput  = Input of a fluid (can be null, and respects StackSize)
-     * @param aFluidOutput = Output of the Molten Metal (not null, and respects StackSize)
+     * @param aInputFluid  = Input of a fluid (can be null, and respects StackSize)
+     * @param aOutput      = Output of the Molten Metal (not null, and respects StackSize)
      * @param aOutputStack = Item Output (Can be null)
-     * @param aChances     = Output Chance (can be == 0)
+     * @param aChance      = Output Chance (can be == 0)
      * @param aDuration    = Duration (must be >= 0)
      * @param aEUt         = EU per tick needed for heating up (must be >= 0)
      * @return true if the Recipe got added, otherwise false.
@@ -161,10 +122,10 @@ public interface IGregtech_RecipeAdder {
      * Adds a Recipe for the Alloy Blast Smelter. (up to 9 Inputs, More than 1 fluids)
      *
      * @param aInput       = ItemStack[] (not null, and respects StackSize)
-     * @param aFluidInput  = FluidStack[] (can be null, and respects StackSize)
-     * @param aFluidOutput = Output of the Molten Metal (not null, and respects StackSize)
+     * @param aInputFluid  = FluidStack[] (can be null, and respects StackSize)
+     * @param aOutput      = Output of the Molten Metal (not null, and respects StackSize)
      * @param aOutputStack = Item Output (Can be null)
-     * @param aChances     = Output Chance (can be == 0)
+     * @param aChance      = Output Chance (can be == 0)
      * @param aDuration    = Duration (must be >= 0)
      * @param aEUt         = EU per tick needed for heating up (must be >= 0)
      * @return true if the Recipe got added, otherwise false.
@@ -183,10 +144,10 @@ public interface IGregtech_RecipeAdder {
      * Adds a Recipe for the LFTRr. (up to 9 Inputs)
      *
      * @param aInput        = ItemStack[] (not null, and respects StackSize)
-     * @param aFluidInput   = Input of a fluid (can be null, and respects StackSize)
-     * @param aFluidOutput  = Output of the Molten Salts (not null, and respects StackSize)
+     * @param aInputFluid   = Input of a fluid (can be null, and respects StackSize)
+     * @param aOutput       = Output of the Molten Salts (not null, and respects StackSize)
      * @param aOutputStack  = Item Output (Can be null)
-     * @param aChances      = Output Chance (can be == 0)
+     * @param aChance       = Output Chance (can be == 0)
      * @param aDuration     = Duration (must be >= 0)
      * @param aEUt          = EU per tick needed for heating up (must be >= 0)
      * @param aSpecialValue = Power produced in EU/t per dynamo
@@ -207,10 +168,10 @@ public interface IGregtech_RecipeAdder {
      * Adds a Recipe for the LFTRr. (up to 9 Inputs, More than 1 fluids)
      *
      * @param aInput        = ItemStack[] (not null, and respects StackSize)
-     * @param aFluidInput   = FluidStack[] (can be null, and respects StackSize)
-     * @param aFluidOutput  = Output of the Molten Salts (not null, and respects StackSize)
+     * @param aInputFluid   = FluidStack[] (can be null, and respects StackSize)
+     * @param aOutput       = Output of the Molten Salts (not null, and respects StackSize)
      * @param aOutputStack  = Item Output (Can be null)
-     * @param aChances      = Output Chance (can be == 0)
+     * @param aChance       = Output Chance (can be == 0)
      * @param aDuration     = Duration (must be >= 0)
      * @param aEUt          = EU per tick needed for heating up (must be >= 0)
      * @param aSpecialValue = Power produced in EU/t per dynamo
@@ -218,13 +179,6 @@ public interface IGregtech_RecipeAdder {
      */
     public boolean addBlastSmelterRecipe(ItemStack[] aInput, FluidStack[] aInputFluid, FluidStack aOutput,
             ItemStack[] aOutputStack, int[] aChance, int aDuration, int aEUt, int aSpecialValue);
-
-    public boolean addLFTRRecipe(ItemStack aInput1, ItemStack aInput2, ItemStack aOutput1, int aDuration, int aEUt);
-
-    public boolean addLFTRRecipe(ItemStack aInput1, FluidStack aInput2, ItemStack aOutput1, FluidStack aOutput2,
-            int aDuration, int aEUt);
-
-    public boolean addLFTRRecipe(FluidStack aInput1, FluidStack aInput2, FluidStack aOutput1, int aDuration, int aEUt);
 
     /**
      * Adds a custom Semifluid fuel for the GT++ SemiFluid Generators.
@@ -284,12 +238,6 @@ public interface IGregtech_RecipeAdder {
     public boolean addAdvancedFreezerRecipe(ItemStack[] aInputs, FluidStack[] aFluidInputs, FluidStack[] aFluidOutputs,
             ItemStack[] aOutputs, int[] aChances, int aDuration, int aEUtick, int aSpecial);
 
-    public boolean addAssemblerRecipeWithOreDict(Object aInput1, int aAmount1, Object aInput2, int aAmount2,
-            ItemStack aOutput, int a1, int a2);
-
-    public boolean addAssemblerRecipeWithOreDict(Object aInput1, int aAmount1, Object aInput2, int aAmount2,
-            FluidStack aInputFluid, ItemStack aOutput, int a1, int a2);
-
     public boolean addSixSlotAssemblingRecipe(ItemStack[] aInputs, FluidStack aInputFluid, ItemStack aOutput1,
             int aDuration, int aEUt);
 
@@ -298,7 +246,7 @@ public interface IGregtech_RecipeAdder {
      *
      * @param aInputs      must be != null, 4-16 inputs
      * @param aFluidInputs 0-4 fluids
-     * @param aOutput1     must be != null
+     * @param aOutput      must be != null
      * @param aDuration    must be > 0
      * @param aEUt         should be > 0
      */
@@ -352,47 +300,6 @@ public interface IGregtech_RecipeAdder {
     public boolean addFluidCannerRecipe(ItemStack aContainer, ItemStack aFullContainer, FluidStack rFluidIn,
             FluidStack rFluidOut, int aTime, int aEu);
 
-    /**
-     * Adds a Fusion reactor Recipe
-     *
-     * @param aInput1                = first Input (not null, and respects StackSize)
-     * @param aInput2                = second Input (not null, and respects StackSize)
-     * @param plasma                 = Output of the Fusion (can be null, and respects StackSize)
-     * @param aFusionDurationInTicks = How many ticks the Fusion lasts (must be > 0)
-     * @param aEu                    = The EU generated per Tick (can even be negative!)
-     * @param aSpecial               = EU needed for heating the Reactor up (must be >= 0)
-     */
-    public boolean addFusionReactorRecipe(ItemStack aInputStackA, ItemStack aInputStackB, FluidStack plasma,
-            int aFusionDurationInTicks, int aEu, int aSpecial);
-
-    /**
-     * Adds a Fusion reactor Recipe
-     *
-     * @param aInput1                = first Input (not null, and respects StackSize)
-     * @param aInput2                = second Input (not null, and respects StackSize)
-     * @param plasma                 = Output of the Fusion (can be null, and respects StackSize)
-     * @param aOutputChance          = chance to output plasma (can be 0)
-     * @param aFusionDurationInTicks = How many ticks the Fusion lasts (must be > 0)
-     * @param aEu                    = The EU generated per Tick (can even be negative!)
-     * @param aSpecial               = EU needed for heating the Reactor up (must be >= 0)
-     */
-    public boolean addFusionReactorRecipe(FluidStack aInputStackA, FluidStack aInputStackB, FluidStack plasma,
-            int aOutputChance, int aFusionDurationInTicks, int aEu, int aSpecial);
-
-    /**
-     * Adds a Fusion reactor Recipe
-     *
-     * @param aInput1                = first Input (not null, and respects StackSize)
-     * @param aInput2                = second Input (not null, and respects StackSize)
-     * @param plasma                 = Output of the Fusion (can be null, and respects StackSize)
-     * @param aOutputChance          = chance to output plasma (can be 0)
-     * @param aFusionDurationInTicks = How many ticks the Fusion lasts (must be > 0)
-     * @param aEu                    = The EU generated per Tick (can even be negative!)
-     * @param aSpecial               = EU needed for heating the Reactor up (must be >= 0)
-     */
-    public boolean addFusionReactorRecipe(ItemStack aInputStackA, ItemStack aInputStackB, FluidStack plasma,
-            int aOutputChance, int aFusionDurationInTicks, int aEu, int aSpecial);
-
     public boolean addVacuumFurnaceRecipe(ItemStack aInput1, ItemStack aInput2, FluidStack aFluidInput,
             FluidStack aFluidOutput, ItemStack aOutput1, ItemStack aOutput2, int aDuration, int aEUt, int aLevel);
 
@@ -400,8 +307,6 @@ public interface IGregtech_RecipeAdder {
             FluidStack[] aFluidOutputs, int aDuration, int aEUt, int aLevel);
 
     public boolean addUvLaserRecipe(ItemStack aInput1, ItemStack aOutput, int time, long eu);
-
-    public boolean addIrLaserRecipe(ItemStack aInput1, ItemStack aOutput, int time, long eu);
 
     public boolean addChemicalPlantRecipe(ItemStack[] aInputs, FluidStack[] aInputFluids, ItemStack[] aOutputs,
             FluidStack[] aFluidOutputs, int time, long eu, int aTier);
