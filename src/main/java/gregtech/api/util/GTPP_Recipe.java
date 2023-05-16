f960b9a293d40a80a3223a2fcc8fc12e678db2c1
梅天佑 <52530814+MeiTianyou@users.noreply.github.com>
2023-05-16 12:03:58 +0800

    Change TGS info in NEI to reflect true behavior
    Should close #11650 in GTNewHorizons/GT-New-Horizons-Modpack

--- src/main/java/gregtech/api/util/GTPP_Recipe.java
+++ src/main/java/gregtech/api/util/GTPP_Recipe.java
@@ -818,15 +818,14 @@ public class GTPP_Recipe extends GT_Recipe implements IComparableRecipe {
                 }
                 return currentTip;
             }
-
+            
             @Override
             protected void drawNEIOverlayForOutput(FixedPositionedStack stack) {}
         }.useModularUI(true).setNEISpecialInfoFormatter((recipeInfo, applyPrefixAndSuffix) -> {
             List<String> result = new ArrayList<>();
             if (ModItems.fluidFertBasic != null) {
-                result.add("Sapling is outputted if");
+                result.add("The Sapling is not consumed. Additional saplings are outputted instead of wood if");
                 result.add("" + ModItems.fluidFertBasic.getLocalizedName() + " is provided.");
-                result.add("This is optional.");
             }
             return result;
         });
