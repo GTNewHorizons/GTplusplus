09cbf4fc7d97b89f4e37ae63c26fe9d14a534165
chochem <40274384+chochem@users.noreply.github.com>
2023-07-04 10:20:48 +0100

    restore pams compat

---
+++ src/main/java/gtPlusPlus/xmod/pamsharvest/HANDLER_PAMS.java
@@ -0,0 +1,17 @@
+package gtPlusPlus.xmod.pamsharvest;
+
+import static gregtech.api.enums.Mods.PamsHarvestCraft;
+
+
+public class HANDLER_PAMS {
+
+    public static void preInit() {if (PamsHarvestCraft.isModLoaded()) {}}
+
+    public static void init() {
+        if (PamsHarvestCraft.isModLoaded()) {}
+    }
+
+    public static void postInit() {
+        if (PamsHarvestCraft.isModLoaded()) {}
+    }
+}
