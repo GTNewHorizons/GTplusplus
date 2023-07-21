07245a7f310093c9575dde531c09a9c94166231d
minecraft7771 <maxim235@gmx.de>
2023-07-21 15:49:05 +0200

    Derp

--- src/main/java/gtPlusPlus/xmod/gregtech/common/tileentities/machines/multi/processing/GregtechMetaTileEntity_IndustrialChisel.java
+++ src/main/java/gtPlusPlus/xmod/gregtech/common/tileentities/machines/multi/processing/GregtechMetaTileEntity_IndustrialChisel.java
@@ -29,7 +29,6 @@ import gregtech.api.interfaces.metatileentity.IMetaTileEntity;
 import gregtech.api.interfaces.tileentity.IGregTechTileEntity;
 import gregtech.api.logic.ProcessingLogic;
 import gregtech.api.metatileentity.implementations.GT_MetaTileEntity_Hatch_InputBus;
-import gregtech.api.recipe.check.CheckRecipeResult;
 import gregtech.api.recipe.check.FindRecipeResult;
 import gregtech.api.util.GTPP_Recipe;
 import gregtech.api.util.GT_Multiblock_Tooltip_Builder;
@@ -270,16 +269,6 @@ public class GregtechMetaTileEntity_IndustrialChisel
 
         }
         return null;
-    }
-
-    @NotNull
-    @Override
-    public CheckRecipeResult checkProcessing() {
-        CheckRecipeResult result = super.checkProcessing();
-        if (result.wasSuccessful()) {
-            if (GT_Utility.isStringValid(getSound())) this.sendLoopStart((byte) 1);
-        }
-        return result;
     }
 
     @Override
