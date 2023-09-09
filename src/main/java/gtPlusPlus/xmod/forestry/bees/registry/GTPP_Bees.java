d29a617e8c808083bc370defc0356b1001d9402a
serenibyss <10861407+serenibyss@users.noreply.github.com>
2023-09-09 17:55:11 -0500

    Remove Dragon Blood Pollen: unused, uncraftable

--- src/main/java/gtPlusPlus/xmod/forestry/bees/registry/GTPP_Bees.java
+++ src/main/java/gtPlusPlus/xmod/forestry/bees/registry/GTPP_Bees.java
@@ -10,11 +10,9 @@ import gtPlusPlus.core.material.Material;
 import gtPlusPlus.core.util.reflect.ReflectionUtils;
 import gtPlusPlus.xmod.forestry.bees.handler.GTPP_CombType;
 import gtPlusPlus.xmod.forestry.bees.handler.GTPP_DropType;
-import gtPlusPlus.xmod.forestry.bees.handler.GTPP_PollenType;
 import gtPlusPlus.xmod.forestry.bees.handler.GTPP_PropolisType;
 import gtPlusPlus.xmod.forestry.bees.items.output.GTPP_Comb;
 import gtPlusPlus.xmod.forestry.bees.items.output.GTPP_Drop;
-import gtPlusPlus.xmod.forestry.bees.items.output.GTPP_Pollen;
 import gtPlusPlus.xmod.forestry.bees.items.output.GTPP_Propolis;
 
 public class GTPP_Bees {
@@ -26,13 +24,11 @@ public class GTPP_Bees {
     public static final byte GREGTECH = 4;
 
     public static GTPP_Propolis propolis;
-    public static GTPP_Pollen pollen;
     public static GTPP_Drop drop;
     public static GTPP_Comb combs;
 
     public static HashMap<String, Material> sMaterialMappings = new HashMap<String, Material>();
     public static HashMap<Integer, GTPP_PropolisType> sPropolisMappings = new HashMap<Integer, GTPP_PropolisType>();
-    public static HashMap<Integer, GTPP_PollenType> sPollenMappings = new HashMap<Integer, GTPP_PollenType>();
     public static HashMap<Integer, GTPP_DropType> sDropMappings = new HashMap<Integer, GTPP_DropType>();
     public static HashMap<Integer, GTPP_CombType> sCombMappings = new HashMap<Integer, GTPP_CombType>();
 
@@ -40,7 +36,6 @@ public class GTPP_Bees {
         if (Forestry.isModLoaded() && GT_Mod.gregtechproxy.mGTBees) {
             Logger.BEES("Creating required items.");
             propolis = new GTPP_Propolis();
-            pollen = new GTPP_Pollen();
             drop = new GTPP_Drop();
             combs = new GTPP_Comb();
 
